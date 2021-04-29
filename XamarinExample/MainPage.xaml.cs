ebdd171284cd3ec2137cbb6ecc43dfbedefed86f
11379831 <ekaphop@hotmail.com>
2021-04-30 01:14:55 +0700

    Add Exercise Section2: XAML Essentials

--- XamarinExample/MainPage.xaml.cs
+++ XamarinExample/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinExample.Views.Section2;
 
 namespace XamarinExample
 {
@@ -14,5 +15,11 @@ namespace XamarinExample
         {
             InitializeComponent();
         }
+
+        async void Section2Label_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EssentialExercisePage());
+        }
+
     }
 }
---
+++ XamarinExample/Views/Section2/EssentialExercisePage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace XamarinExample.Views.Section2
+{
+    [XamlCompilation(XamlCompilationOptions.Skip)]
+    public partial class EssentialExercisePage : ContentPage
+    {
+        private int _index = 0;
+        private string[] _quotes = new string[]
+        {
+            "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+            "You can't blame gravity for falling in love.",
+            "Look deep into nature, and then you will understand everything better."
+        };
+
+        public EssentialExercisePage()
+        {
+            InitializeComponent();
+
+            currentQuote.Text = _quotes[_index];
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                stackLayout.Padding = new Thickness(20, 30, 20, 20);
+            }
+            else if (Device.RuntimePlatform == Device.Android)
+            {
+                stackLayout.Padding = new Thickness(20, 30, 20, 20);
+            }
+            else if (Device.RuntimePlatform == Device.UWP)
+            {
+                stackLayout.Padding = new Thickness(20, 40, 20, 20);
+            }
+                
+        }
+
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            _index++;
+            if (_index >= _quotes.Length)
+                _index = 0;
+
+            currentQuote.Text = _quotes[_index];
+        }
+    }
+}
